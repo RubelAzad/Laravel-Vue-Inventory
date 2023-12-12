e12fdcaf3918e10b8e6fd432f86653ba73a2a563
RubelAzad <rubelazad123@gmail.com>
2023-12-12 15:36:53 +0600

    Configure and login, Register page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
-import './bootstrap';
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import { routes } from "./routes";
+
+const app = createApp({
+    el: "#app",
+});
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+app.use(router);
+
+app.mount("#app");
